5ec5e1d8b2e9d1e4f700990118a404d9ad654b09
Ilir Hushi <ilirhushi@gmail.com>
2018-03-13 16:25:36 +0100

    Inital Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,74 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = ' Recursive View using Angular 5';
+
+  public tree = [
+    {
+      title: 'Basketball',
+      children: []
+    },
+    {
+      title: 'Football',
+      children: [
+        {
+          title: 'Liga BBVA',
+          children: []
+        },
+        {
+          title: 'Seria A',
+          children: [
+            {
+              title: 'AC Milan',
+              children: []
+            },
+            {
+              title: 'Juventus',
+              children: []
+            },
+            {
+              title: 'Internazionale Milan',
+              children: [
+                {
+                  title: 'Main Team',
+                  children: []
+                },
+                {
+                  title: 'Youth Team',
+                  children: []
+                },
+              ]
+            },
+            {
+              title: 'Roma',
+              children: [
+                {
+                  title: 'Main Tema',
+                  children: []
+                },
+              ]
+            },
+          ]
+        },
+        {
+          title: 'Premier League',
+          children: [
+            {
+              title: 'Chelsea',
+              children: []
+            },
+            {
+              title: 'Man United',
+              children: []
+            },
+            {
+              title: 'Man City',
+              children: []
+            },
+          ]
+        },
+      ]
+    },
+  ];
+
 }
